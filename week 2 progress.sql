aa2744f9bebe29cc0f1da693770db78c3bd4bf1e
Satwik Konala <satwik.konala@yahoo.com>
2025-11-07 11:51:42 +0530

    Refactor SQL comments in week 2 progress.sql
    Updated SQL comments for clarity and consistency.

--- week 2 progress.sql
+++ week 2 progress.sql
@@ -1,4 +1,5 @@
-- week 2:
+'''
+week 2:
     🔹 Subqueries
     🔹 Window Functions
     🔹 Common Table Expressions (CTEs)
@@ -6,33 +7,33 @@
     🔹 Temporary Tables
     🔹 Joins & Set Operations
 
+'''
 
 
 
-
-# Find employees whose salary is above the company average.
+--Find employees whose salary is above the company average.
 select * from employee_records 
 where salary > (select avg(salary) from employee_records)
 order by salary ;
 
-# Show departments with fewer employees than the average department size.
+--Show departments with fewer employees than the average department size.
 select department, count(*) as total_employees from employee_records
 group by department
 having total_employees < (select AVG(department_wise_employees) from (select count(*) as department_wise_employees from employee_records group by department) as department) 
 order by total_employees;
 
-# List employees who joined after the most recent hire in the 'HR' department.
+--List employees who joined after the most recent hire in the 'HR' department.
 select * from employee_records
 where joining_date > (select joining_date from employee_records where department = 'HR' order by joining_date desc limit 1)
 order by joining_date ;
 
-# Rank employees by salary within each department
+--Rank employees by salary within each department
 select *, rank() over(partition by department order by salary desc) as salary_rank_by_department from employee_records;
 
-# Calculate running total of salaries by department 
+--Calculate running total of salaries by department 
 select *, sum(salary) over(partition by department order by salary desc) as running_total from employee_records ;
 
-# Find the first employee who joined in each department
+--Find the first employee who joined in each department
 with first_employees_by_department as
 (
 select *, dense_rank() over(partition by department order by joining_date) as first_employee from employee_records
@@ -40,22 +41,22 @@ select *, dense_rank() over(partition by department order by joining_date) as fi
 select * from first_employees_by_department
 where first_employee = 1;
 
-# Flag top 3 earners in each department.
+--Flag top 3 earners in each department.
 with salary_ranking as
 (
 select *, rank() over(partition by department order by salary desc ) as salary_rank from employee_records
 )
 select * from salary_ranking where salary_rank < 4;
 
-# Show salary difference between each employee and department average ().
-# Show salary difference between each employee and department average ().
+
+--Show salary difference between each employee and department average ().
 with avg_salary_by_department as
 (
 select *,avg(salary) over(partition by department ) as avg_salary_department_wise from employee_records
 )
 select *,(salary - avg_salary_department_wise) as salary_difference  from avg_salary_by_department ;
 
-#Create a stored procedure to fetch employee details by department name.
+--Create a stored procedure to fetch employee details by department name.
 delimiter $$
 create procedure department_wise_employees (department_name varchar(50))
 begin
